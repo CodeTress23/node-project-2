d3f356e488977255ddb6e47dfa0a2baa739da933
Codestress23 <amber.massaquoi@gmail.com>
2024-03-27 20:21:29 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 4100;
+
+app.get("/", (request, response, next) => {
+    response.send("Welcome to the cafe's homepage!");
+});
+app.get("/menu", (request, response, next) => {
+    response.send("Welcome to the cafe's menu!");
+});
+app.get("/katz", (request, response, next) => {
+    response.send("Welcome to the cafe's cat wall of fame!");
+});
+app.get("/admin", (request, response, next) => {
+    response.send("Hello, this page is for admins only!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Welcome to the cool CodeSquader Cafe! Now loading server...on ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
